23793fc6bdda7e51d7cfc20a14d8486b8524e6ea
abinashsahoo612 <abinashsahoo612@gmail.com>
2026-07-26 18:34:38 +0530

    debugging continue...

--- Program.cs
+++ Program.cs
@@ -14,7 +14,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
     );
 var cs = builder.Configuration.GetConnectionString("DefaultConnection");
-Console.WriteLine($"Connection String: [{cs}]");
+
+Console.WriteLine("========== CONNECTION STRING ==========");
+Console.WriteLine(cs);
+Console.WriteLine("=======================================");
 // builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Identity
